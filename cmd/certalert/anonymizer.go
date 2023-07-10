3a0257d630e55bc44c8d9f30e7a3898b4020a4eb
Michael Kondrashin <mkondrashin@gmail.com>
2023-07-10 19:46:18 +0300

    improve anonymizer

---
+++ cmd/certalert/anonymizer.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"math/big"
+	"regexp"
+	"strings"
+)
+
+func hide(v any) string {
+	s := fmt.Sprintf("%v", v)
+	hasher := sha1.New()
+	hasher.Write([]byte(s))
+	return to64(hasher.Sum(nil)) // fmt.Sprintf("%x", hasher.Sum(nil))
+}
+
+func to64(data []byte) string {
+	var sb strings.Builder
+	v := new(big.Int).SetBytes(data)
+	//fmt.Printf("data = %v\n", v)
+	characters := "0123456789ABCDEFGHIJKLMNOPQRTSUVWXYZabcdefghijklmnopqrstuvwxyz-_"
+
+	length := big.NewInt(int64(len(characters)))
+	m := new(big.Int)
+	for i := 0; i < (len(data)*8+1)/6; i++ {
+		v, m = v.DivMod(v, length, m)
+		c := characters[m.Uint64()]
+		sb.WriteRune(rune(c))
+	}
+	return sb.String()
+}
+
+func maskIPv4(input string) string {
+	//fmt.Printf("Mask %s\n", string(input))
+	numBlock := "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+	regexPattern := numBlock + "\\." + numBlock + "\\." + numBlock + "\\." + numBlock
+	regEx := regexp.MustCompile(regexPattern)
+	return regEx.ReplaceAllStringFunc(input, func(s string) string {
+		//fmt.Printf("IP %s\n", s)
+		return "IP:" + hide(s)
+	})
+}
+
+type Anonymizer struct {
+	target io.Writer
+}
+
+func NewAnonymizer(target io.Writer) Anonymizer {
+	return Anonymizer{
+		target: target,
+	}
+}
+
+func (a Anonymizer) Write(p []byte) (n int, err error) {
+	// fmt.Printf("Anon %s\n", string(p))
+	s := maskIPv4(string(p))
+	return a.target.Write([]byte(s))
+}
