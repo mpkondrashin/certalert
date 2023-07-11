1b9b9f9ade47ef887db66930210f522685e259d6
Michael Kondrashin <mkondrashin@gmail.com>
2023-07-11 21:04:37 +0300

    optimize duplicates

--- cmd/certalert/certdup.go
+++ cmd/certalert/certdup.go
@@ -5,25 +5,20 @@ import (
 	"fmt"
 )
 
-func certKey(cert *x509.Certificate) string {
-	return fmt.Sprintf("%v:%v:%v:%v:%v", cert.SerialNumber, cert.Issuer, cert.Subject, cert.NotBefore, cert.NotAfter)
+type CertDuplicates map[string]struct{}
+
+func NewCertDuplicates() CertDuplicates {
+	return make(CertDuplicates)
+
 }
 
-type CertDuplicatesMap map[string]struct{}
-
-type CertDuplicates struct {
-	seen CertDuplicatesMap
-}
-
-func NewCertDuplicates() *CertDuplicates {
-	return &CertDuplicates{
-		seen: make(CertDuplicatesMap),
-	}
-}
-
-func (s *CertDuplicates) Seen(cert *x509.Certificate) bool {
+func (c CertDuplicates) Seen(cert *x509.Certificate) bool {
 	key := certKey(cert)
-	_, ok := s.seen[key]
-	s.seen[key] = struct{}{}
+	_, ok := c[key]
+	c[key] = struct{}{}
 	return ok
+}
+
+func certKey(cert *x509.Certificate) string {
+	return fmt.Sprintf("%v:%v:%v", cert.SerialNumber, cert.Issuer, cert.Subject)
 }
